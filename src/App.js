0c6d4e44904d2ab8672712263fb18ec8f92a34ba
devMarilia <devMarilia.frontend@gmail.com>
2022-07-02 22:08:49 -0300

    ✨ feat: navbar init completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import MainRoutes from "./pages/routes";
+import GlobalStyle from "./styles/globalStyles";
 
 function App() {
   return (
     <BrowserRouter>
+    <GlobalStyle/>
+      <Navbar/>
       <MainRoutes/>
     </BrowserRouter>
   );
